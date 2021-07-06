eeab93fb3486e6df75e5a5a3c5274f4b8169a4d2
Logic2019 <samuellamlptey@icloud.com>
2021-07-06 09:34:16 +0000

    new commit

--- api/dist/app.js
+++ api/dist/app.js
@@ -1,27 +1,29 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const product_1 = require("./product");
-const app = express_1.default();
-const port = 3001;
-function getProduct() {
-    let p = new product_1.Product();
-    p.Id = "1";
-    p.Price = 100;
-    p.Title = "Cricket Bat";
-    p.inStock = true;
-    return p;
+const { MongoClient } = require("mongodb");
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://logic:<password>@messagingapp.ioymf.mongodb.net/messagingappdb?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            console.log("Connected correctly to server");
+        }
+        catch (err) {
+            console.log(err.stack);
+        }
+        finally {
+            yield client.close();
+        }
+    });
 }
-app.get('/products', (req, res) => {
-    res.send(getProduct());
-});
-app.listen(port), (err) => {
-    if (err) {
-        return console.error(err);
-    }
-    return console.log(`server is listening on ${port}`);
-};
+run().catch(console.dir);
 //# sourceMappingURL=app.js.map
